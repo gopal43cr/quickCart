5b8e7c25af371e1252b4a872feb89e8555c04157
Gopal Das <114994218+gopal43cr@users.noreply.github.com>
2023-11-23 14:50:00 +0530

    Update RegBasicExp.jsx

--- src/RegBasicExp.jsx
+++ src/RegBasicExp.jsx
@@ -81,7 +81,7 @@ export default function RegBasicExample() {
                 <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
                   Don't have an account?{" "}
                   <Link
-    to="/login"  // Set the correct path for your login page
+    to="/"  // Set the correct path for your login page
     className="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700"
   >
     Login
